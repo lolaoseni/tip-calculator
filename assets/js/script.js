0c66bbc9e2509cedfca2a7c7982f43bdc3b67a5d
lolaoseni <lolaa.oseni@gmail.com>
2022-01-25 16:31:33 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let  d = document;
+
+
+
+// Get total bill from input
+
+
+// Select tip %
+
+// Enter number of people
+// const getPeople = () => {
+//     let people = document.getElementById('numOfPeople').value;
+//     return people;
+// }
+// Calculate tip : bill * %
+
+// Calculate Tip per person: Total/ Num of people
+
+// Bill Total: Bill + Tip
+
+// Each Person Total: Bill Total / Num of People
+
+// Display Tip Amount.
+
+//Display Each person's total.
